68cb44e5d8344d11664f4eafb098c17a10bc7f1f
conn22 <49536271+conn22@users.noreply.github.com>
2019-11-05 09:05:41 -0800

    Updated QuitAction
    Added the necessary file imports and built a constructor

--- app/src/main/java/edu/johnsong22/pazaak/QuitAction.java
+++ app/src/main/java/edu/johnsong22/pazaak/QuitAction.java
@@ -1,4 +1,12 @@
 package edu.johnsong22.pazaak;
 
-public class QuitAction {
+import edu.johnsong22.pazaak.GameFramework.GamePlayer;
+import edu.johnsong22.pazaak.GameFramework.actionMessage.GameAction;
+
+public class QuitAction extends GameAction{
+  
+  public QuitAction(GamePlayer player){
+    super(player);
+  }
+  
 }
