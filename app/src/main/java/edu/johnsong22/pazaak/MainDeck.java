c44a49429e0cdc487834bf10bb40dcd34760398b
conn22 <49536271+conn22@users.noreply.github.com>
2019-11-05 09:00:38 -0800

    Updated MainDeck
    fixed the constructor

--- app/src/main/java/edu/johnsong22/pazaak/MainDeck.java
+++ app/src/main/java/edu/johnsong22/pazaak/MainDeck.java
@@ -7,18 +7,7 @@ Glen Johnson, Jim Rowe, Grant Stone, James Conn
 public class MainDeck extends Cards {
 
     public MainDeck () {
-        this.value = (int) (Math.random()*8+1);
-        this.type = 0;
-    }
-
-    @Override
-    public int getValue() {
-        return super.getValue();
-    }
-
-    @Override
-    public int getType() {
-        return super.getType();
+        super((int) (Math.random()*10+1), false);
     }
 
 }
