2b129b4048c06e3466bd10523ce15f5aa687600b
conn22 <49536271+conn22@users.noreply.github.com>
2019-11-19 16:24:39 -0800

    Update Card constructors
    Reorganized the card class and added a proper side deck card constructor

--- app/src/main/java/edu/johnsong22/pazaak/Card.java
+++ app/src/main/java/edu/johnsong22/pazaak/Card.java
@@ -10,14 +10,32 @@ public class Card {
     protected boolean fromSideDeck;
     protected boolean isFlippable;
     protected boolean isNegitive;
-
+    
+    //constructor to randomly generate a main card
     public Card() {
         value = (int) (Math.random()*9)+1;
         fromSideDeck = false;
         isFlippable = false;
         isNegitive = false;
     }
-
+    
+    //constructor for a side deck card
+    public Card(int v, boolean sD, boolean flip, boolean neg) {
+        value = v;
+        fromSideDeck = sD;
+        isFlippable = flip;
+        isNegitive = neg;
+    }
+    
+    //deepcopy constructor
+    public Card(Card c) {
+        this.value = c.getValue();
+        this.fromSideDeck = c.fromSideDeck();
+        this.isNegitive = c.isNegitive();
+        this.isFlippable = c.isFlippable();
+    }
+    
+    //randomly generates a side deck card (for alpha version)
     public Card(boolean sD) {
         value = (int) (Math.random()*5 )+1;
         fromSideDeck = true;
@@ -34,21 +52,21 @@ public class Card {
             }
         }
     }
-
+    
+    //creates a basic side deck card (for testing)
     public Card(int v, boolean sD) {
         value = v;
         fromSideDeck = sD;
         isFlippable = false;
         isNegitive = false;
     }
-
-    public Card(Card c) {
-        this.value = c.getValue();
-        this.fromSideDeck = c.fromSideDeck();
-        this.isNegitive = c.isNegitive();
-        this.isFlippable = c.isFlippable();
+    
+    //flips the value of a card, if the card is flippable
+    public void flip () {
+        value = value * -1;
     }
 
+    //getters for class attributes
     public int getValue() {
         return value;
     }
@@ -63,10 +81,6 @@ public class Card {
 
     public boolean isNegitive() {
         return isNegitive;
-    }
-
-    public void flip () {
-        value = value * -1;
     }
 
 }
